afa5c2d9e46735ec8171c0cf77249951a702d828
bard <schy2503@naver.com>
2023-06-06 19:09:09 +0900

    2.3 Collection For

---
+++ dart_tour/2. Data Types/2.3 Collection For.dart
@@ -0,0 +1,14 @@
+void main() {
+  var oldFriends = ["nico", "lynn"];
+  var newFriends = [
+    "lewis",
+    "ralph",
+    "darren",
+    for (var friend in oldFriends) "old Friend: $friend",
+  ];
+
+  // for (var friend in oldFriends) {
+  //   newFriends.add("old Friend: $friend");
+  // }
+  print(newFriends);
+}
